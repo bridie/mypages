8245b176d9132a5db23efbf258b9c46a9f096646
Bridie <bridie.dibble@gmail.com>
2016-03-12 18:09:11 +0000

    Using Grunt to compile less files to CSS.

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "public/stylesheets/style.css": "assets/stylesheets/main.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['assets/stylesheets/*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
