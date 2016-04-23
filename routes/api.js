5aaefec137751834b6f7a34cbf468a6f6ab785f4
Bridie <bridie.dibble@gmail.com>
2016-04-23 19:18:24 +0100

    When a page is added it gets automatically added to the miscellaneous group. Groups api routes added.

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 var Page = require('../models/page');
+var Group = require('../models/group');
 
 module.exports = function(router) {
 
@@ -15,7 +16,73 @@ module.exports = function(router) {
       }
     });
 
+    // All pages when first created get added to the 'miscellaneous' group.
+    Group.findOne({ name: 'miscellaneous'}, function(err, group) {
+      if (err) {
+        res.send(err);
+      }
+
+      if (group) {
+          group.pages.push(page);
+      } else {
+        // Create the 'miscellaneous' group if it does not exist already.
+        group = new Group({
+          name: 'miscellaneous',
+          pages: [page]
+        });
+      }
+
+      group.save(function(err) {
+        if (err) {
+          res.send(err);
+        }
+      });
+
+    });
+
     res.json({'message': 'Page added'});
+  });
+
+  router.get('/api/pages', function(req, res) {
+
+    Page.find(function(err, pages) {
+
+      if (err) {
+        res.send(err);
+      }
+
+      res.json(pages);
+    });
+
+  });
+
+  router.post('/api/groups', function(req, res) {
+
+    var group = new Group({
+      name: req.body.pageName,
+      pages: []
+    });
+
+    group.save(function(err) {
+      if (err) {
+        res.send(err);
+      }
+    });
+
+    res.json({'message': 'Group added'});
+
+  });
+
+  router.get('/api/groups', function(req, res) {
+
+    Group.find(function(err, groups) {
+
+      if (err) {
+        res.send(err);
+      }
+
+      res.json(groups);
+    });
 
   });
 
